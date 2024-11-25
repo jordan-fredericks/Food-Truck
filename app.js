d91c0844b881e97a12337cbb9492c212c4ff2cf0
Fredericks <500172971@fvtc.edu>
2024-11-25 15:43:22 -0600

    working

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+// app.use(middleware)
+app.use(express.json())
+app.use(express.static('public'))
+
+app.use('/api/v1', require('./routes/api/v1/foodtruck'))
+app.use(require('./routes/static'))
+
+
+app.listen(port, () => console.log(`http://localhost:${port}`))
